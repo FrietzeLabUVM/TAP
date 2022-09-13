6e6303a6a614bc955517f1ea2213b1b850a142e4
jrboyd <boydjr7@gmail.com>
2022-09-13 14:09:30 -0400

    23055 typo in rnaseq_pipeline.sh

--- rnaseq_pipeline.sh
+++ rnaseq_pipeline.sh
@@ -154,7 +154,7 @@ align_jid=$(parse_jid "$align_qsub")
 echo align_jid $align_jid
 
 #rDNA alignment
-if [ -d $rDNA_index ] && [ ! -z $$rDNA_index ] ; then
+if [ -d $rDNA_index ] && [ ! -z $rDNA_index ] ; then
   align_rDNA_path=${align_path}/rDNA
   mkdir -p $align_rDNA_path
   rdna_qsub=$($qsub_cmd -J STAR_rDNA $SCRIPTS/run_STAR.rDNA.sh -f1 $F1 -wd $align_rDNA_path -idx $rDNA_index -o ${root}.rDNA -f1s $F1_suff -f2s $F2_suff $se_mode)
