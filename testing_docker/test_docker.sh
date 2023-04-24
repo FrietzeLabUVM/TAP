ad6b7d06ff878af7a7e4df63f3eb28f349716108
jrboyd <boydjr7@gmail.com>
2023-04-24 09:44:08 -0400

    test_docker now adapts to script path and tests singularity

--- testing_docker/test_docker.sh
+++ testing_docker/test_docker.sh
@@ -1,21 +1,60 @@
-rm -r /slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_alignment.pool2
+dock_test_dir=$(dirname "$0")
+dock_test_dir=$(readlink -f $dock_test_dir)
+scripts=$(dirname $dock_test_dir)
+#dock_test_dir=$scripts/testing_docker
+#cd $dock_test_dir
+test_dir=${scripts}/testing
+
+#docker environment
+if ! command -v docker &> /dev/null
+then
+echo docker is not available
+else
+rm -r ${test_dir}/test_alignment.pool2
 bash run_STAR.noSort.sh \
-  -f1 "/slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_data/test_dm6_1_R1_001.fastq.gz&/slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_data/test_dm6_2_R1_001.fastq.gz" \
-  -wd /slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_alignment.pool2 \
-  -idx /slipstream/home/joeboyd/indexes/DM6/STAR_INDEX \
+  -f1 "${test_dir}/test_data/test_dm6_1_R1_001.fastq.gz&${test_dir}/test_data/test_dm6_2_R1_001.fastq.gz" \
+  -wd ${test_dir}/test_alignment.pool2 \
+  -idx ~/lab_shared/indexes/DM6/STAR_INDEX \
   -o pool_1and2.withDocker \
   -f1s _R1_001.fastq.gz \
   -f2s _R2_001.fastq.gz \
-  --docker tap 
+  --docker jrboyd/tap 
 
-ls -lha /slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_alignment.pool2/pool_1and2.withDocker*bam
+ls -lha ${test_dir}/test_alignment.pool2/pool_1and2.withDocker*bam
+fi
 
+#singularity environment
+if ! command -v singularity &> /dev/null
+then
+echo singularity is not available
+else
+rm -r ${test_dir}/test_alignment.pool2
 bash run_STAR.noSort.sh \
-  -f1 "/slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_data/test_dm6_1_R1_001.fastq.gz&/slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_data/test_dm6_2_R1_001.fastq.gz" \
-  -wd /slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_alignment.pool2 \
-  -idx /slipstream/home/joeboyd/indexes/DM6/STAR_INDEX \
+  -f1 "${test_dir}/test_data/test_dm6_1_R1_001.fastq.gz&${test_dir}/test_data/test_dm6_2_R1_001.fastq.gz" \
+  -wd ${test_dir}/test_alignment.pool2 \
+  -idx ~/lab_shared/indexes/DM6/STAR_INDEX \
+  -o pool_1and2.withSingularity \
+  -f1s _R1_001.fastq.gz \
+  -f2s _R2_001.fastq.gz \
+  --docker jrboyd/tap  \
+  --useSingularity
+
+ls -lha ${test_dir}/test_alignment.pool2/pool_1and2.withSingularity*bam
+fi
+
+#native environment
+if ! command -v STAR &> /dev/null
+then
+echo STAR is not available
+else
+bash run_STAR.noSort.sh \
+  -f1 "${test_dir}/test_data/test_dm6_1_R1_001.fastq.gz&${test_dir}/test_data/test_dm6_2_R1_001.fastq.gz" \
+  -wd ${test_dir}/test_alignment.pool2 \
+  -idx ~/lab_shared/indexes/DM6/STAR_INDEX \
   -o pool_1and2.noDocker \
   -f1s _R1_001.fastq.gz \
   -f2s _R2_001.fastq.gz
 
-ls -lha /slipstream_old/home/joeboyd/lab_shared/scripts/vacc_rnaseq_pipeline/testing/test_alignment.pool2/pool_1and2.noDocker*bam
+ls -lha ${test_dir}/test_alignment.pool2/pool_1and2.noDocker*bam
+fi
+
