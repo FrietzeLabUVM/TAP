5064a0716893bfb1742401a6f309983febb486f0
jrboyd <boydjr7@gmail.com>
2023-09-28 15:40:48 -0400

    deploy parses config

--- deployment/deploy_TAP_scripts.sh
+++ deployment/deploy_TAP_scripts.sh
@@ -12,7 +12,7 @@ in_param="$@"
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -h|--help) cat $SCRIPT_PATH/help_msg.txt; exit 0; shift ;;
+        -h|--help) cat "$SCRIPT_PATH"/help_msg.txt; exit 0; shift ;;
     esac
     shift
 done
@@ -21,15 +21,17 @@ done
 set -- ${in_param}
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -c|--config) cfg="$2"; if [ ! -f $cfg ]; then echo cannnot find config file $cfg. quit!; exit 1; fi; shift ;;
+        -c|--config) cfg="$2"; if [ ! -f $cfg ]; then echo "cannnot find config file: \"$cfg\". quit!"; exit 1; fi; shift ;;
     esac
     shift
 done
+if [ -z "$cfg" ]; then echo "config file (-c|--config) is required."; exit 1; fi
 
-jh=$SCRIPT_PATH/job_headers
-jt=$SCRIPT_PATH/job_templates
-dt=$SCRIPT_PATH/../deployed_job_scripts
+jh=job_headers
+jt=job_templates
+dt=../deployed_job_scripts
 force=false
+paths_relative=true
 
 #parse args specified in config file by lines starting with #CFG
 if [ -n "$cfg" ]; then
@@ -43,9 +45,10 @@ if [ -n "$cfg" ]; then
       case $1 in
         -c|--config) echo ignoring config file specified in config file.; shift ;;
         -f|--force) force=true; ;;
-        --job_headers) jh=job_headers; shift ;;
-        --job_templates) jt=job_templates; shift ;;
-        --deployment_target) dt=../deployed_job_scripts; shift ;;
+        --job_headers) jh="$2"; shift ;;
+        --job_templates) jt="$2"; shift ;;
+        --deployment_target) dt="$2"; shift ;;
+        --paths_relative) paths_relative=$2; shift;;
         *) echo "Unknown parameter passed: $1"; cat "$SCRIPT_PATH"/help_msg.txt; exit 1 ;;
         esac
       shift
@@ -59,19 +62,28 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         -c|--config) shift ;;
         -f|--force) force=true; ;;
-        --job_headers) jh=job_headers; shift ;;
-        --job_templates) jt=job_templates; shift ;;
-        --deployment_target) dt=../deployed_job_scripts; shift ;;
+        --job_headers) jh="$2"; shift ;;
+        --job_templates) jt="$2"; shift ;;
+        --deployment_target) dt="$2"; shift ;;
+        --paths_relative) paths_relative=$2; shift;;
         *) echo "Unknown parameter passed: $1"; cat "$SCRIPT_PATH"/help_msg.txt; exit 1 ;;
     esac
     shift
 done
+
+if [ "$paths_relative" = "true" ]; then
+    jh="$SCRIPT_PATH/$jh"
+    jt="$SCRIPT_PATH/$jt"
+    dt="$SCRIPT_PATH/$dt"
+fi
 
 echo deployment config file is "$cfg"
 echo job_headers directory is "$jh"
 echo job_templates directory is "$jt"
 echo deployment_target directory is "$dt"
 echo force is $force
+
+
 
 if [ ! -d "$jh" ]; then echo "job_headers directory \($jh\) not found!"; exit 1; fi
 if [ ! -d "$jh" ]; then echo "job_templates directory \($jt\) not found!"; exit 1; fi
@@ -84,3 +96,19 @@ if [ -d "$dt" ]; then
 fi
 
 mkdir -p "$dt"
+
+todo=$(cat "$cfg" | awk '/^[^#]/ {print $0 }')
+
+
+for f1 in $todo; do
+  #template_script,job_name,header,processors
+  temp_script=$(echo $f1 | awk -v FS="," '{print $1}');
+  job_name=$(echo $f1 | awk -v FS="," '{print $2}');
+  header_file=$(echo $f1 | awk -v FS="," '{print $3}');
+  n_proc=$(echo $f1 | awk -v FS="," '{print $4}');
+  echo "  temp_script $temp_script"
+  echo "  job_name $job_name"
+  echo "  header_file $header_file"
+  echo "  n_proc $n_proc"
+
+done
